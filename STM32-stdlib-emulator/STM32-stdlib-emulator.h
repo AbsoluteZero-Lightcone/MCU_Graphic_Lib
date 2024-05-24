5ab52a19c36a24b970d4bb836aeaff238b1611da
Lightcone <AbsoluteZeroStudio@outlook.com>
2024-05-25 00:11:08 +0800

    stm32-stdlib-emulator

---
+++ STM32-stdlib-emulator/STM32-stdlib-emulator.h
@@ -0,0 +1,15 @@
+/**
+  ******************************************************************************
+  * @file    STM32-stdlib-emulator.h
+  * @author  Lightcone
+  * @version V1.0.0
+  * @date    2024-05-25
+  * @brief   STM32标准库模拟器
+  ******************************************************************************
+  */
+#ifndef __STM32_STDLIB_EMULATOR_H
+#define __STM32_STDLIB_EMULATOR_H
+
+
+
+#endif /* __STM32_STDLIB_EMULATOR_H */
---
+++ STM32-stdlib-emulator/main.c
@@ -0,0 +1,18 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Lightcone
+  * @version V1.0.0
+  * @date    2024-05-25
+  * @brief   STM32标准库模拟器 入口函数
+  ******************************************************************************
+  */
+
+#include "STM32-stdlib-emulator.h"
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, World!\n");
+    return 0;
+}
